c9e3e9bb9a1014898f74406f341dca47bfab4cae
yosuke <44027481+yosuke1979@users.noreply.github.com>
2019-02-08 20:11:00 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -21,7 +21,7 @@ Highcharts.chart('chart', {
                 }
                 series.addPoint([x, py], true, true);
               });
-          }, 3000);
+          }, 10000);
         } 
       }
     },
