d42cbf2b5d074dd2dc189d3eb8040437582d5210
tomotomo <tomotomobile@gmail.com>
2019-02-08 15:39:44 +0900

    min yAxis

--- static/script.js
+++ static/script.js
@@ -16,8 +16,8 @@ Highcharts.chart('chart', {
                     py = res.body.pressure,
                     ty = res.body.temperature;
                 
-                if (py < series.yAxis.oldMin | !series.yAxis.oldMin) {
-                    series.yAxis.update({min: py});
+                if (ty < series.yAxis.oldMin | !series.yAxis.oldMin) {
+                    series.yAxis.update({min: ty});
                 }
                 series.addPoint([x, ty], true, true);
               });
